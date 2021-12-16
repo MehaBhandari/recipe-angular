63e2a4a88b7e78615e2245b293ec41e97c655a23
Meha <ahem4progs@gmail.com>
2021-12-16 16:31:55 +0530

    Added service

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit, OnDestroy } from "@angular/core";
 import { from, of} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
+import { RecipeService } from './recipe.service';
 
 @Component({
     selector: 'app-recipe',
@@ -8,52 +9,21 @@ import {map, tap} from 'rxjs/operators';
 })
 
 export class RecipeComponent implements OnInit, OnDestroy {
+  constructor(private recipeService: RecipeService) {}
   private recipeObservable;
-    private recipeList = [
-        {
-          image: 'https://www.vegrecipesofindia.com/wp-content/uploads/2020/01/chia-pudding-280x280.jpg',
-          name: 'Chia Pudding (Easy, Vegan & Healthy)',
-          description: 'Chia pudding is my easy breakfast option on busy mornings. You will love this chia pudding made with coconut milk and coconut sugar.'
-    
-        }, {
-          image: 'https://www.vegrecipesofindia.com/wp-content/uploads/2016/08/avocado-garlic-bread-toast.jpg',
-          name: 'Avocado Toast',
-          description: 'Every home cook needs a good basic Avocado toast recipe for a quick, healthy, and satisfying breakfast or snack.'
-    
-        }, {
-          image: 'https://www.vegrecipesofindia.com/wp-content/uploads/2015/05/oats-porridge-recipe-quick.jpg',
-          name: 'Oats Porridge',
-          description: 'Oats porridge recipe with step by step pics – quick oats porridge made with quick-cooking oats or rolled oats.'
-    
-        }, {
-          image: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/idli-recipe-1-1024x1536.jpg',
-          name: 'Idli',
-          description: 'Idli is a soft, pillowy steamed savory cake made from rice and lentil batter. The lentils used in making idli are urad dal (black gram).'
-    
-        }, {
-          image: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/07/dosa-recipe-1-1024x1536.jpg',
-          name: 'Dosa',
-          description: 'Dosa is the ever-popular South Indian breakfast of crispy crepes made with fermented rice and lentil batter.'
-    
-        }, {
-          image: 'https://www.vegrecipesofindia.com/wp-content/uploads/2009/08/aloo-paratha-recipe-4.jpg',
-          name: 'Aloo Paratha',
-          description: 'Aloo paratha is a popular Indian breakfast of unleavened whole wheat savory and spiced potato stuffed flatbread from Punjab, India.'
-    
-    }];
-
-    ngOnInit() {        
-    this.fetchRecipeList();
+  public recipeList: [];
+  ngOnInit() {        
+  this.fetchRecipeList();
+  }
+  fetchRecipeList() {
+    this.recipeObservable = this.recipeService.fetchRecipeList().pipe(
+      tap(console.log)
+    ).subscribe((recipeListArray) => {
+      this.recipeList = recipeListArray
     }
-    fetchRecipeList() {
-        this.recipeObservable = from(this.recipeList).pipe(
-          tap(console.log)
-        ).subscribe(
-          recipeList => recipeList
-        )
-    }
-    
-    ngOnDestroy() {
-      this.recipeObservable.unsubscribe();
+    )
+  }  
+  ngOnDestroy() {
+    this.recipeObservable.unsubscribe();
     }
 }
